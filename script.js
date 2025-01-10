fc0bb981934f3b46f9c61f0da878debff86fa68a
Elisei <eliseivlas.002@gmail.com>
2025-01-10 16:40:40 +0100

    Consegna esercizio Bonus

---
+++ script.js
@@ -0,0 +1,27 @@
+const foto = document.getElementById("immagine")
+
+const foto2 = document.getElementById("immagine2")
+
+const button = document.getElementById("pulsante")
+
+button.addEventListener("click" ,
+    function () {
+
+    // COMPITO DI BASE
+        // foto.classList.add("close");
+        // foto2.classList.remove("close")
+        // button.innerHTML = "Accendi"
+
+    // BONUS
+        if(button.innerHTML === "Spegni"){
+            button.innerHTML = "Accendi" 
+            foto.classList.add("close");
+            foto2.classList.remove("close")
+        }else{
+            button.innerHTML = "Spegni"
+            foto.classList.remove("close");
+            foto2.classList.add("close")
+        }
+
+    }
+);
