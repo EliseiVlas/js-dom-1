b7b5df8875629d72612841d74e1ec138f24947f5
Elisei <eliseivlas.002@gmail.com>
2025-01-10 17:58:41 +0100

    aggiunta colore pulsante

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+// Selezione dei elementi
 const foto = document.getElementById("immagine")
 
 const foto2 = document.getElementById("immagine2")
 
 const button = document.getElementById("pulsante")
 
+// gestione dell'evento
 button.addEventListener("click" ,
     function () {
 
@@ -13,16 +15,20 @@ button.addEventListener("click" ,
         // button.innerHTML = "Accendi"
 
     // BONUS
+        // cambio foto, scritta e colore quando avviene il "click"
         if(button.innerHTML === "Accendi"){
             button.innerHTML = "Spegni" 
             // foto.classList.add("close");
             // foto2.classList.remove("close")
+            // button.style.backgroundColor = "red"
         }else{
             button.innerHTML = "Accendi"
             // foto.classList.remove("close");
             // foto2.classList.add("close")
+            // button.style.backgroundColor = "greenyellow"
         }
         foto.classList.toggle("close")
         foto2.classList.toggle("active")
+        button.classList.toggle("color")
     }
 );
